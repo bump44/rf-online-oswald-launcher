f11c46b1823e9f8ee994df7282e48dc2674fb0e6
Ilya Gerasimov <r0sky1@yandex.ru>
2019-04-06 21:02:21 +0300

    Oswald development config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-export const GAME_CLIENT_UPDATE_SERVER_URL = 'http://localhost:9091';
-export const SOCKET_CONNECTION_URL = 'http://localhost:8091';
+export const GAME_CLIENT_UPDATE_SERVER_URL = 'http://84.201.138.131:8002';
+export const SOCKET_CONNECTION_URL = 'http://84.201.138.131:8001';
 export const PROGRAM_UPDATE_SERVICE_URL = 'http://localhost:5000';
 export const ACCOUNT_URL = 'https://rf-oswald.ru';
 export const FORUM_URL = 'https://forum.rf-oswald.ru';
